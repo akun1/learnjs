e0df4f85bdb4327661e4f3a83b94360ba5db5a5f
Akash Kundu <akashkundu92@gmail.com>
2018-06-14 22:07:45 -0500

    getting started with tests

---
+++ public/tests/app_spec.js
@@ -0,0 +1,7 @@
+//first test 
+describe('LearnJS', function() {
+	it('can show a problem view', function() {
+		learnjs.showView('#problem-1');
+		expect($('.view-container .problem-view').length).toEqual(1);
+	});
+});
