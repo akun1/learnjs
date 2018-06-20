e9043497ddc9e7cb604f791bc8f5b66916329114
Akash Kundu <akashkundu92@gmail.com>
2018-06-19 22:05:08 -0500

    added a few more tests, working on router

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,7 +1,27 @@
-//first test 
 describe('LearnJS', function() {
+//first test to test is there is 1 thing inside of view-container class AND in problem-view class
 	it('can show a problem view', function() {
 		learnjs.showView('#problem-1');
 		expect($('.view-container .problem-view').length).toEqual(1);
 	});
+//second test to test if landing page shows when no hash provided
+	it('shows the landing page when there is no hash', function() {
+		learnjs.showView('');
+		expect($('.view-container .landing-view').length).toEqual(1);
+	});
+//third test spies on problemView function to see if '42' was passed to it
+	it('passes the hash view parameter to the view function', function() {
+		spyOn(learnjs, 'problemView');
+		learnjs.showView('#problem-42');
+		expect(learnjs.problemView).toHaveBeenCalledWith('42');
+	});
+//this is a new context called 'problem view' within the larger 'LearnJS' context
+	describe('problem view', function() {
+//4th test tests to see if problem number is in result of problem view function		
+		it('has a title that includes the problem number', function() {
+			var view = learnjs.problemView('1');
+			expect(view.text()).toEqual('Problem #1 Coming Soon!')
+		});
+	}); 
+
 });
